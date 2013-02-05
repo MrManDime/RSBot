43f2dcb6f84af14433dd9ef78c3655c994778c8c
Harold N <haroldnieuwb@hotmail.nl>
2013-02-05 17:15:19 +0100

    Working on new flipper

---
+++ src/org/harrynoob/scripts/autoflipper/misc/Constants.java
@@ -0,0 +1,38 @@
+package org.harrynoob.scripts.autoflipper.misc;
+
+public class Constants {
+	
+	public static final int WIDGET_GE_BASE = 105;
+	
+	public static final int WIDGET_PROGRESS_BAR = 12;
+	
+	public enum GESlot {
+		
+		ONE(29, 30, 1),
+		TWO(45, 46, 2);
+		
+		private final int sellWidgetId;
+		private final int buyWidgetId;
+		private final int slotNumber;
+		
+		GESlot(final int widgetSell, final int widgetBuy, final int slotNum) {
+			//this.sellWidgetId = widgetSell;
+			//this.buyWidgetId = widgetBuy;
+			this.slotNumber = slotNum;
+			this.sellWidgetId = 29 + (slotNum - 1) * 16;
+			this.buyWidgetId = 30 + (slotNum - 1) * 16;
+		}
+		
+		public int getSellWidgetId() {
+			return sellWidgetId;
+		}
+		
+		public int getBuyWidgetId() {
+			return buyWidgetId;
+		}
+	}
+	
+	
+	
+
+}
